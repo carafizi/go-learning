314a2867d46c36909688c6169334b947240a822b
Andrei Carafizi <carafizi@gmail.com>
2018-02-11 20:09:55 +0100

    testing

--- hello.go
+++ hello.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt" 
+	"math/rand"
+)
+var k = 3
+
+func add(x, y int) int {
+	return x + y
+}
 
 func main() {
-    fmt.Printf("hello, world\n")
+	z := 4
+	fmt.Println(k, z)
+	fmt.Println("My favorite number is", rand.Intn(100))	
+	fmt.Println(add(42, 13))
 }
